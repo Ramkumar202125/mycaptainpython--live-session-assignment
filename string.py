7f388e606004450efdfa69388352aedcdf6fe4cb
Ramkumar202125 <103440009+Ramkumar202125@users.noreply.github.com>
2022-05-08 10:39:23 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,61 @@
+a="hello world"
+print(a)
+
+#ACCESSING CHARACTER IN STRING
+print(a[2])
+print(a[-1])
+
+#replacing string
+print(a.replace("o","e"))
+
+#deleting string
+del a
+
+
+b="hello world"
+#string concadenation (+)
+c="hello"
+d="world"
+print(c+d)
+
+#string append (+=)
+c+=" python "
+print(c)
+
+#string repeating
+print(c*3)
+
+#string slicing
+print(c[0:5])
+
+#length if string
+print(len(c))
+
+#capitalise()
+print(c.capitalize())
+
+#center
+print(c.center(15,"*"))
+
+#swapcase
+print(c.swapcase())
+
+#upper()
+print(c.upper())
+
+#lower(;)
+print(c.lower())
+
+#count
+print(c.count("o"))
+
+print(c.islower())
+
+print(c.isupper())
+
+print(c.isalpha())
+
+print(c.isalnum())
+
+print(c.isdigit())
+
